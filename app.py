bc829504304eb5ce9ac436b8c483b31616fa6bfa
rhawn <rhawn@maxwellhealth.com>
2016-03-02 16:40:14 -0500

    prettifying the .py script

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis, RedisError
 import os
 import socket
 
 # Connect to Redis
-redis = Redis(host=os.getenv('REDISSERVER'), db=0)
+redis = Redis(host="redis", db=0)
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
+def printme():
     try:
         visits = redis.incr('counter')
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits = "cannot connect to Redis, counter disabled"
+    return visits
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Should be connecting to redis server at:</b> {redisserv}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv('NAME', "world"), redisserv=os.getenv('REDISSERVER', "redis server env variable not set"), hostname=socket.gethostname(), visits=visits)
-
+@app.route("/")
+def hello():
+    name = os.getenv('NAME', "world")
+    redis = os.getenv('REDISSERVER')
+    hostname = socket.gethostname()
+    # visitcount = printme()
+    return render_template('page.html', name=name, redis=redis, hostname=hostname, visitcount=printme())
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
